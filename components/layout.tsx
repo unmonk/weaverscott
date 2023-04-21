73ad99b2d4ff1d36a670dfe17dce9ff1ca6bae3e
Scott Weaver <scott@weaverscott.com>
2023-04-21 12:23:43 -0500

    initial commit

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,23 +1,45 @@
-import Alert from './alert'
-import Footer from './footer'
-import Meta from './meta'
+import Alert from "./alert";
+import Footer from "./footer";
+import Meta from "./meta";
+import Sidebar from "./sidebar";
 
 type Props = {
-  preview?: boolean
-  children: React.ReactNode
-}
+  preview?: boolean;
+  children: React.ReactNode;
+};
 
 const Layout = ({ preview, children }: Props) => {
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
+      <div className="min-h-screen bg-base-100 drawer drawer-mobile">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <Sidebar />
+        <div className="drawer-content flex flex-col bg-base-100">
+          <div className="md:flex md:justify-center">
+            <main className="p-6 pt-10 max-w-[900px]">{children}</main>
+          </div>
+          <Footer />
+        </div>
       </div>
-      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
+
+{
+  /* <div class="bg-base-100 drawer drawer-mobile">
+  <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col bg-base-100">
+    <Header title={SITE_TITLE} />
+    <div class="md:flex md:justify-center">
+      <main class="p-6 pt-10 max-w-[900px]">
+        <slot />
+      </main>
+    </div>
+    <Footer />
+  </div>
+  {includeSidebar && <SideBar />}
+</div>; */
+}
