be723c75875ee381dd888952b3702f2718fad44e
Scott Weaver <scott@weaverscott.com>
2023-07-11 15:41:56 -0500

    fixed footer colors

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ interface FooterProps {}
 
 const Footer: FC<FooterProps> = ({}) => {
   return (
-    <footer className="flex flex-row items-center justify-end w-full p-2 border-t border-gray-200 dark:border-gray-800 gap-2">
+    <footer className="flex flex-row items-center justify-end w-full p-2 border-t border-t-secondary gap-2">
       Scott Weaver &copy; {new Date().getFullYear()}
       <ThemeToggle />
     </footer>
