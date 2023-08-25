8b668fcfffac895177e5604a01297abb6cffd2df
Scott Weaver <scott@weaverscott.com>
2023-08-25 17:59:21 -0500

    photo gradient

--- components/about.tsx
+++ components/about.tsx
@@ -9,7 +9,7 @@ const About: FC<AboutProps> = ({}) => {
     <section className="flex flex-col max-w-4xl">
       <h2 className="text-2xl font-semibold">About Me</h2>
       <div className=" rounded-xl border-primary relative">
-        <div className="bg-gradient-to-tr from-gray-300 via-background to-gray-400 w-full h-full rounded-xl blur-xl hover:blur-2xl hover:opacity-30 opacity-20 absolute"></div>
+        <div className="bg-gradient-to-br from-gray-300 via-background to-gray-400 w-full h-full rounded-xl blur-xl hover:blur-2xl hover:opacity-30 opacity-20 absolute"></div>
         {/* About me area */}
         {aboutMeParagraphs.map((paragraph) => (
           <p className="text-left p-4 md:text-lg" key={paragraph}>
--- components/header.tsx
+++ components/header.tsx
@@ -10,10 +10,13 @@ const Header: FC<HeaderProps> = ({}) => {
   return (
     <section className="mb-6">
       <div className="flex flex-col md:flex-row items-center justify-center">
-        <Avatar className="h-24 w-24 mr-2 ">
-          <AvatarImage src={profile.image} alt={profile.name} />
-          <AvatarFallback>SW</AvatarFallback>
-        </Avatar>
+        <div>
+          <div className="h-28 w-28 absolute rounded-full bg-gradient-radial from-emerald-300 via-emerald-900 to-background blur-lg opacity-75 animate-gradient" />
+          <Avatar className="h-24 w-24 mr-2 inset-0">
+            <AvatarImage src={profile.image} alt={profile.name} />
+            <AvatarFallback>SW</AvatarFallback>
+          </Avatar>
+        </div>
 
         <h1 className="text-4xl md:text-6xl font-bold">{profile.name}</h1>
       </div>
